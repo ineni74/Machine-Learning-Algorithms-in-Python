f0bf5ed97114777bab6405a41c3509e93921d1ee
sreenivasarao ineni <ineni74@gmail.com>
2022-09-17 14:30:34 +0530

    Update creating pandas data frame from csv file.py

--- creating pandas data frame from csv file.py
+++ creating pandas data frame from csv file.py
@@ -4,5 +4,5 @@ with open("file_name") as csvfile:
     csvReader = csv.reader(csvfile, delimiter=',')
     for row in csvReader:
         csv_data.append(row)
-        print(row)
+
 data_df = pd.DataFrame(csv_data[1:], columns=csv_data[0])
